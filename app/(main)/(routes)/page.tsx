db897362d9d4ff82f23d8a18abe05bb2706b6fab
Rakesh Valasala <105775467+VALASALARAKESH@users.noreply.github.com>
2024-04-14 18:01:52 +0530

    Design Theme for the application

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import {UserButton} from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
 export default function Home() {
   return (
       <div >
           <UserButton afterSignOutUrl="/">
 
           </UserButton>
+          <ModeToggle></ModeToggle>
       </div>
 
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
-import { Open_Sans} from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import {cn} from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -16,10 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <ClerkProvider>
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-      </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={ cn(font.className, "bg-white dark:bg-[#313338]") }>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+
+            storageKey="dark-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
