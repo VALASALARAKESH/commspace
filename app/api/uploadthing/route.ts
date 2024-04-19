6b3be1b780580ce347550eccc4ddc9b1ed6e35af
Rakesh Valasala <105775467+VALASALARAKESH@users.noreply.github.com>
2024-04-19 08:11:50 +0530

    set all the dependencies

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,8 @@
-import { createRouteHandler } from "uploadthing/next";
-
+import { createNextRouteHandler } from "uploadthing/next";
+ 
 import { ourFileRouter } from "./core";
-
-export const { GET, POST } = createRouteHandler({
-    router: ourFileRouter,
-
+ 
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
+  router: ourFileRouter,
 });
--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -1,14 +1,19 @@
-import {auth} from "@clerk/nextjs";
-import {db} from "@/lib/db";
-export  const currentProfile=async()=>{
-    const {userId}=auth();
-    if(!userId){
-        return null;
-    }
-    const profile=await db.profile.findUnique({
-        where:{
-            userId
-        }
-    });
-    return profile;
-}
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const currentProfile = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return profile;
+};
